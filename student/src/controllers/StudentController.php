571a1284d0fb74eb2dffd677eb86325581ff0c05
myanmarlinks <myanmarlinks.net@gmail.com>
2014-01-25 19:20:20 +0630

    Add Detail Blade

--- student/src/controllers/StudentController.php
+++ student/src/controllers/StudentController.php
@@ -8,56 +8,72 @@ class StudentController extends BaseController {
 
 	public function allStudents() {
 		$sql = 'SELECT 
-			students.id,
-			students.name,
-			students.address,
-			classes.name AS class_name
-			FROM students, classes
-			WHERE students.class_id = classes.id';
+		students.id,
+		students.name,
+		students.address,
+		classes.name AS class_name
+		FROM students, classes
+		WHERE students.class_id = classes.id';
 		$students = DB::select($sql);
 		return View::make('student::allstudents')
-			->with('students', $students);
+		->with('students', $students);
 	}
 
 	public function phpFoundation() {
 		$sql = 'SELECT 
-			students.id,
-			students.name,
-			students.address,
-			classes.name AS class_name
-			FROM students, classes
-			WHERE students.class_id = classes.id
-			AND students.class_id = 1';
+		students.id,
+		students.name,
+		students.address,
+		classes.name AS class_name
+		FROM students, classes
+		WHERE students.class_id = classes.id
+		AND students.class_id = 1';
 		$students = DB::select($sql);
 		return View::make('student::allstudents')
-			->with('students', $students);
+		->with('students', $students);
 	}
 	public function phpAdvanced() {
 		$sql = 'SELECT 
-			students.id,
-			students.name,
-			students.address,
-			classes.name AS class_name
-			FROM students, classes
-			WHERE students.class_id = classes.id
-			AND students.class_id = 2';
+		students.id,
+		students.name,
+		students.address,
+		classes.name AS class_name
+		FROM students, classes
+		WHERE students.class_id = classes.id
+		AND students.class_id = 2';
 		$students = DB::select($sql);
 		return View::make('student::allstudents')
-			->with('students', $students);
+		->with('students', $students);
 	}
 
 	public function detail($id) {
 		$sql = 'SELECT students.id,
-			students.name,
-			students.address,
-			classes.name AS class_name
-			FROM students, classes
-			WHERE students.class_id = classes.id
-			AND students.id = ?';
+		students.name,
+		students.address,
+		classes.name AS class_name
+		FROM students, classes
+		WHERE students.class_id = classes.id
+		AND students.id = ?';
+		$sql2 = 'SELECT interests.name AS interest_name
+		FROM students_interests, interests
+		WHERE students_interests.interest_id = interests.id
+		AND students_interests.student_id = ?
+		AND interests.type = ?';
+		$sql3 = 'SELECT experiences.id, experiences.name AS exp_name,
+				experiences.from,
+				experiences.to
+				FROM experiences
+				WHERE experiences.student_id = ?';
 		$students = DB::select($sql, array($id));
+		$scripts = DB::select($sql2, array($id, 'script'));
+		$languages = DB::select($sql2, array($id, 'language'));
+		$experiences = DB::select($sql3, array($id));
 		if(count($students) == 1) {
 			return View::make('student::detail')
-				->with('students', $students);
+			->with('students', $students)
+			->with('scripts', $scripts)
+			->with('languages', $languages)
+			->with('experiences', $experiences);
 		} else {
 			return View::make('student::404');
 		}
@@ -65,4 +81,4 @@ class StudentController extends BaseController {
 }
 
 
- ?>
+?>
--- student/src/views/detail.blade.php
+++ student/src/views/detail.blade.php
@@ -2,46 +2,63 @@
 
 @section('content')
 <div id="content" class="row">
- 		<div class="span12">
- 		 			<table class="table table-striped">
- 				<tr>
- 					<td>ID#</td>
- 				<td>{{ $students[0]->id }}</td>
- 				</tr>
- 				<tr>
- 					<td>Name</td>
- 					<td>{{ $students[0]->name }}</td>
- 				</tr>
- 				<tr>
- 					<td>Address</td>
- 					<td>{{ $students[0]->address }}</td>
- 				</tr>
- 				<tr>
- 					<td>Class</td>
- 					<td>{{ $students[0]->class_name }}</td>
- 				</tr>
- 				<tr>
- 					<td>Interest - Script</td>
- 					<td>
- 						 							PHP,  						 							HTML,  						 							CSS,  						 					</td>
- 					</tr>
- 					<tr>
- 						<td>Interest - Language</td>
- 						<td>
- 							 							C++,  						 							C#,  						 							VB.NET,  						 						</td>
- 						</tr>
- 					</table>
- 					<h3>Experience</h3>
- 					<table class="table table-striped">
- 						<tr>
- 							<td>Myanmar Tutorials</td>
- 							<td>From - 1993  To - 2013</td>
- 						</tr>
- 						<tr>
- 							<td>Myanmar Tutors</td>
- 							<td>From - 2012  To - 2013</td>
- 						</tr>
- 					</table>
- 				 				</div>
- 			</div> <!-- end of content -->
+	<div class="span12">
+		<table class="table table-striped">
+			<tr>
+				<td>ID#</td>
+				<td>{{ $students[0]->id }}</td>
+			</tr>
+			<tr>
+				<td>Name</td>
+				<td>{{ $students[0]->name }}</td>
+			</tr>
+			<tr>
+				<td>Address</td>
+				<td>{{ $students[0]->address }}</td>
+			</tr>
+			<tr>
+				<td>Class</td>
+				<td>{{ $students[0]->class_name }}</td>
+			</tr>
+			<tr>
+				<td>Interest - Script</td>
+				<td>
+					@foreach($scripts as $script)
+					{{ $script->interest_name }},
+					@endforeach
+				</td>
+			</tr>
+			<tr>
+				<td>Interest - Language</td>
+				<td>
+					@foreach($languages as $language)
+					{{ $language->interest_name }},
+					@endforeach
+				</td>	
+			</tr>
+		</table>
+		<h3>Experience</h3>
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>No.</th>
+					<th>Place Name</th>
+					<th>From</th>
+					<th>To</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($experiences as $experience)
+					<tr>
+						<td>{{ $experience->id }}</td>
+						<td>{{ $experience->exp_name }}</td>
+						<td>{{ $experience->from }}</td>
+						<td>{{ $experience->to }}</td>
+					</tr>
+				@endforeach
+			</tbody>
+
+		</table>
+	</div>
+</div> <!-- end of content -->
 @stop
